59682033b659555c5f0c5d98570861b64349375a
Adam2053 <arpitmarathe2002@gmail.com>
2026-02-04 05:46:33 +0530

    .gitignore added

--- backend/utils/sendAuthToken.js
+++ backend/utils/sendAuthToken.js
@@ -1,10 +1,26 @@
 import { generateAccessToken, generateRefreshToken } from "./token.js";
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 
 const sendAuthToken = async (res, user) => {
+  try {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-    const currUser = await User.findById(user._id);
-    await user.save();
-}
+    await User.findByIdAndUpdate(user._id, {
+      refresh_token: refreshToken,
+    });
+
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 100,
+    });
+
+    return accessToken;
+  } catch (e) {
+    throw new Error(`${e.message}`);
+  }
+};
+
+export default sendAuthToken;
